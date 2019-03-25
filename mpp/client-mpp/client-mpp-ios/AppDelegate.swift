35bb435b4138ef966eeb9c44cbaf632bfaaeb305
Eduard Bosch <ebosch@habitissimo.com>
2019-03-25 15:07:10 +0100

    feat: Add Firebase Performance to make Ktor crash

--- mpp/client-mpp/client-mpp-ios/AppDelegate.swift
+++ mpp/client-mpp/client-mpp-ios/AppDelegate.swift
@@ -1,11 +1,14 @@
 import UIKit
 import client_mpp
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
         return true
     }
 
